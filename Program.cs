66ee6cf41dd163a0aa8c978b7e75bde061488ae4
LuisSid55 <luiscitty4@gmail.com>
2021-09-21 20:14:02 -0300

    Alterei o texto para meu nome em uma caixa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("| LuisSid55 |");
+            Console.WriteLine("+-----------+");
+            Console.WriteLine();
+        }
+    }
+}
